3c5cad2d37863be5a0bff376ddc75c49539fe5d3
Jhonier Riascos Zapata <jhonierrz@gmail.com>
2023-09-12 12:33:07 -0500

    Create file exercise1.py
    A new file is added with a simple python exercise that reads and returns the first and last name

---
+++ exercise1.py
@@ -0,0 +1,7 @@
+# Solicitar el nombre y apellido
+nombre = input("Ingresa tu nombre: ")
+apellido = input("Ingresa tu apellido: ")
+
+# Mostrar el nombre y apellido
+print(f"Nombre: {nombre}")
+print(f"Apellido: {apellido}")
