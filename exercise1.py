369b169dd15cf1ff919867db41eda5560aa9187c
Jhonier Riascos Zapata <jhonierrz@gmail.com>
2023-09-12 13:21:13 -0500

    Update with improvements
    Improved code to get a list of data from different people

--- exercise1.py
+++ exercise1.py
@@ -1,7 +1,30 @@
-# Solicitar el nombre y apellido
-nombre = input("Ingresa tu nombre: ")
-apellido = input("Ingresa tu apellido: ")
+# Crear una lista para almacenar los datos de las personas
+personas = []
 
-# Mostrar el nombre y apellido
-print(f"Nombre: {nombre}")
-print(f"Apellido: {apellido}")
+# Definir una función para ingresar los datos de una persona
+def ingresar_persona():
+    nombre = input("Ingresa el nombre: ")
+    apellido = input("Ingresa el apellido: ")
+    edad = input("Ingresa la edad: ")
+
+    # Crear un diccionario con los datos de la persona
+    persona = {"Nombre": nombre, "Apellido": apellido, "Edad": edad}
+
+    # Agregar el diccionario a la lista de personas
+    personas.append(persona)
+
+# Solicitar datos de varias personas
+while True:
+    ingresar = input("¿Deseas ingresar datos de una persona? (s/n): ")
+    if ingresar.lower() != "s":
+        break
+    ingresar_persona()
+
+# Mostrar los datos de todas las personas
+print("\nDatos de las personas:")
+for idx, persona in enumerate(personas, start=1):
+    print(f"Persona {idx}:")
+    print(f"Nombre: {persona['Nombre']}")
+    print(f"Apellido: {persona['Apellido']}")
+    print(f"Edad: {persona['Edad']}")
+    print()
